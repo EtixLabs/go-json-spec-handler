539b003a0369cbc787b314d7c95ffdcc76f933d4
sgleizes <stephane.gleizes@gmail.com>
2016-07-24 17:58:15 +0200

    #11 Add TopLevelAction and use POST for action requests

--- client/action.go
+++ client/action.go
@@ -1,50 +1,59 @@
 package jsc
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
-	"strings"
 
 	"github.com/EtixLabs/go-json-spec-handler"
 )
 
-// Action performs an outbound GET /resource/:id/action request
-func Action(baseURL string, resourceType string, id string, action string) (*jsh.Document, *http.Response, error) {
-	request, err := ActionRequest(baseURL, resourceType, id, action)
+// TopLevelAction performs an outbound POST /action request
+func TopLevelAction(baseURL, action string, payload jsh.Sendable) (*jsh.Document, *http.Response, error) {
+	request, err := TopLevelActionRequest(baseURL, action, payload)
 	if err != nil {
 		return nil, nil, err
 	}
-
 	return Do(request, jsh.ObjectMode)
 }
 
 /*
-ActionRequest returns a fully formatted JSONAPI Action (GET /resource/:id/action) request.
+TopLevelActionRequest returns a fully formatted JSONAPI Action (POST /action) request.
+Useful if you need to set custom headers before proceeding. Otherwise just use "jsh.TopLevelAction".
+*/
+func TopLevelActionRequest(baseURL, action string, payload jsh.Sendable) (*http.Request, error) {
+	if action == "" {
+		return nil, errors.New("Action specifier cannot be empty for an Action request type")
+	}
+	u, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing URL: %v", err)
+	}
+	setPath(u, action)
+	return postRequest(u, payload)
+}
+
+// Action performs an outbound POST /resource/:id/action request
+func Action(baseURL, resourceType, id, action string, payload jsh.Sendable) (*jsh.Document, *http.Response, error) {
+	request, err := ActionRequest(baseURL, resourceType, id, action, payload)
+	if err != nil {
+		return nil, nil, err
+	}
+	return Do(request, jsh.ObjectMode)
+}
+
+/*
+ActionRequest returns a fully formatted JSONAPI Action (POST /resource/:id/action) request.
 Useful if you need to set custom headers before proceeding. Otherwise just use "jsh.Action".
 */
-func ActionRequest(baseURL string, resourceType, id string, action string) (*http.Request, error) {
-	if id == "" {
-		return nil, jsh.SpecificationError("ID cannot be empty for an Action request type")
-	}
-
+func ActionRequest(baseURL, resourceType, id, action string, payload jsh.Sendable) (*http.Request, error) {
 	if action == "" {
-		return nil, jsh.SpecificationError("Action specifier cannot be empty for an Action request type")
+		return nil, errors.New("Action specifier cannot be empty for an Action request type")
 	}
-
-	u, urlErr := url.Parse(baseURL)
-	if urlErr != nil {
-		return nil, jsh.ISE(fmt.Sprintf("Error parsing URL: %s", urlErr.Error()))
+	u, err := fetchURL(baseURL, resourceType, id, action)
+	if err != nil {
+		return nil, err
 	}
-
-	setIDPath(u, resourceType, id)
-
-	// concat the action the end of url.Path, ensure no "/" prefix
-	if strings.HasPrefix(action, "/") {
-		action = action[1:]
-	}
-
-	u.Path = strings.Join([]string{u.Path, action}, "/")
-
-	return NewRequest("GET", u.String(), nil)
+	return postRequest(u, payload)
 }
