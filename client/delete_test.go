994e0d3e78e84c956013df771fd5fde1f62dfe7b
sgleizes <stephane.gleizes@gmail.com>
2016-07-24 17:57:15 +0200

    #11 Add PostMany and DeleteMany

--- client/delete_test.go
+++ client/delete_test.go
@@ -5,12 +5,13 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/EtixLabs/go-json-spec-handler"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestDelete(t *testing.T) {
 
-	Convey("DELETE Tests", t, func() {
+	Convey("Delete Tests", t, func() {
 
 		api := testAPI()
 		server := httptest.NewServer(api)
@@ -23,6 +24,14 @@ func TestDelete(t *testing.T) {
 
 			So(err, ShouldBeNil)
 			So(resp.StatusCode, ShouldEqual, http.StatusNoContent)
+		})
+
+		Convey("->DeleteMany()", func() {
+			object := jsh.NewIDObject("foos", "1")
+			json, resp, postErr := DeleteMany(baseURL, "tests", "1", "foos", jsh.IDList{object})
+			So(postErr, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, http.StatusNoContent)
+			So(json, ShouldBeNil)
 		})
 	})
 }
--- client/post.go
+++ client/post.go
@@ -18,7 +18,6 @@ func Post(baseURL string, object *jsh.Object) (*jsh.Document, *http.Response, er
 	if err != nil {
 		return nil, nil, err
 	}
-
 	return Do(request, jsh.ObjectMode)
 }
 
@@ -28,20 +27,43 @@ func Post(baseURL string, object *jsh.Object) (*jsh.Document, *http.Response, er
 func PostRequest(baseURL string, object *jsh.Object) (*http.Request, error) {
 	u, err := url.Parse(baseURL)
 	if err != nil {
-		return nil, fmt.Errorf("Error parsing URL: %s", err.Error())
+		return nil, fmt.Errorf("Error parsing URL: %v", err)
 	}
-
 	setPath(u, object.Type)
+	return postRequest(u, object)
+}
 
-	request, err := NewRequest("POST", u.String(), nil)
+// PostMany allows a consumer to perform a POST /resources/:id/relationships/relationship request.
+func PostMany(baseURL, resourceType, id, relationship string, list jsh.IDList) (*jsh.Document, *http.Response, error) {
+	request, err := PostManyRequest(baseURL, resourceType, id, relationship, list)
 	if err != nil {
-		return nil, fmt.Errorf("Error building POST request: %s", err.Error())
+		return nil, nil, err
 	}
+	return Do(request, jsh.ObjectMode)
+}
 
-	err = prepareBody(request, object)
+// PostManyRequest returns a fully formatted request with JSON body for performing
+// a JSONAPI POST on a relationship. This is useful for if you need to set custom headers on the
+// request. Otherwise just use "jsc.PostMany".
+func PostManyRequest(baseURL, resourceType, id, relationship string, list jsh.IDList) (*http.Request, error) {
+	u, err := fetchURL(baseURL, resourceType, id, "relationships/"+relationship)
 	if err != nil {
 		return nil, err
 	}
+	return postRequest(u, list)
+}
 
+// postRequest creates a fully formatted POST request with JSON body from the given URL and object.
+func postRequest(u *url.URL, payload jsh.Sendable) (*http.Request, error) {
+	request, err := NewRequest("POST", u.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error building POST request: %v", err)
+	}
+	if payload != nil {
+		err = prepareBody(request, payload)
+		if err != nil {
+			return nil, err
+		}
+	}
 	return request, nil
 }
